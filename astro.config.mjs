498dbda7d46d598d47ecaceef00f418c82815157
MW-SAND <s1142173@student.windesheim.nl>
2026-05-03 21:01:57 +0200

    chore: configure custom domain jetlag.3mgroup.eu

--- astro.config.mjs
+++ astro.config.mjs
@@ -50,17 +50,17 @@ export default defineConfig({
                     "Automatically generate maps for Jet Lag The Game: Hide and Seek with ease! Simply name the questions and watch the map eliminate hundreds of possibilities in seconds.",
                 icons: [
                     {
-                        src: "/JetLagHideSeek/JLIcon.png",
+                        src: "/JLIcon.png",
                         sizes: "1080x1080",
                         type: "image/png",
                     },
                     {
-                        src: "/JetLagHideSeek/android-chrome-192x192.png",
+                        src: "/android-chrome-192x192.png",
                         sizes: "192x192",
                         type: "image/png",
                     },
                     {
-                        src: "/JetLagHideSeek/android-chrome-512x512.png",
+                        src: "/android-chrome-512x512.png",
                         sizes: "512x512",
                         type: "image/png",
                     },
@@ -72,6 +72,5 @@ export default defineConfig({
     devToolbar: {
         enabled: false,
     },
-    site: "https://mw-sand.github.io",
-    base: "JetLagHideSeek",
+    site: "https://jetlag.3mgroup.eu",
 });
